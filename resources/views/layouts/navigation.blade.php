8caf382461788ce11ab668318d9919327ae9edb6
Chu, Stephen C-L <scchuab@connect.ust.hk>
2024-05-01 21:10:20 +0800

    first commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,16 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('upload')" :active="request()->routeIs('upload')">
+                        {{ __('Upload File') }}
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('show')" :active="request()->routeIs('show')">
+                        {{ __('Show File') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/upload', function () {
+    return view('file/index');
+})->middleware(['auth', 'verified'])->name('upload');
+
+Route::get('/show', function () {
+    return view('file/show');
+})->middleware(['auth', 'verified'])->name('show');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
